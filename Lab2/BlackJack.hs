597b36efda56ce15811f13be3b95e1ddb44baebf
Michael FAGNO <michael.fagno@gmail.com>
2012-11-03 16:47:47 +0100

    Ex 3.2 done

---
+++ Lab2/BlackJack.hs
@@ -0,0 +1,12 @@
+{-
+ 3.2
+	size hand2	
+							=	size (Add (Card (Numeric 2) Hearts)
+								(Add (Card Jack Spades) Empty))
+
+							= 1 + size (Add (Card Jack Spades) Empty)
+							= 1 + 1 + size(Empty)
+							= 1 + 1 + 0
+							= 2
+-}
+
