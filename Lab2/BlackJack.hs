066eeab9d513a119011cc057f9cc979861d868ce
jonas <jonasbru@wanadoo.fr>
2012-11-06 17:30:45 +0100

    modifs lab2, finitions

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -1,3 +1,7 @@
+-- Functional Programming -- Lab Assignment 2A 
+-- Michael Fagno && Jonas Bru
+-- The file was run through hlint without warnings.
+
 {-
  3.2
 	size hand2	
@@ -14,6 +18,14 @@ module BlackJack where
 import Cards
 import Wrapper
 
+--Hands to use
+hand1 = Add (Card (Numeric 2) Hearts) (Add (Card Jack Spades) Empty) --value 12
+hand2 = Add (Card Ace Hearts) (Add (Card Jack Spades) Empty)         --value 21
+hand3 = Add (Card Ace Hearts) (Add (Card Ace Spades) Empty)          --value 2
+hand4 = Add (Card Jack Hearts) (Add (Card Jack Spades) 
+	(Add (Card Jack Spades) Empty) ) --value 30
+
+--Functions
 empty :: Hand
 empty = Empty
 
@@ -26,7 +38,7 @@ value h | rv <= 21 || na == 0 = rv
 
 rawValue :: Hand -> Integer
 rawValue Empty = 0
-rawValue (Add c h) = (valueCard c) + rawValue h
+rawValue (Add c h) = valueCard c + rawValue h
 
 
 valueRank :: Rank -> Integer
@@ -34,20 +46,23 @@ valueRank (Numeric x) = x
 valueRank Ace	= 11
 valueRank otherwise = 10
 
+
 valueCard :: Card -> Integer
 valueCard c = valueRank (rank c)
+
 
 numberOfAces :: Hand -> Integer
 numberOfAces Empty = 0
 numberOfAces (Add card hand)
-	| rank card == Ace = 1 + numberOfAces(hand)
-	| otherwise = numberOfAces(hand)
+	| rank card == Ace = 1 + numberOfAces hand
+	| otherwise = numberOfAces hand
+
 
 gameOver :: Hand -> Bool
-gameOver hand =	value hand > 11
+gameOver hand =	value hand > 21
 
 
-winner :: Hand → Hand → Player
+winner :: Hand -> Hand -> Player
 winner hPlayer hBank 	| gameOver hPlayer || (not (gameOver hBank) && vhb >= vhp) = Bank
 						| otherwise = Guest
 	where	vhp = value hPlayer;
