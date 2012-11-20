fb95e478aba38600261dda70a75879c3c283713a
jonas <jonasbru@wanadoo.fr>
2012-11-20 13:37:56 +0100

    lab 2 bug fix

--- Lab2/BlackJack.hs
+++ Lab2/BlackJack.hs
@@ -152,7 +152,7 @@ c `belongsTo` Empty	= False
 c `belongsTo` (Add c' h) = c == c' || c `belongsTo` h
 
 prop_size_shuffle :: StdGen -> Hand -> Bool
-prop_size_shuffle g h = size h == size (shuffle (mkStdGen g) h)
+prop_size_shuffle g h = size h == size (shuffle g h)
 
 
 --Interface and main method--------------------------------------------------
