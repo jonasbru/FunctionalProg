bd8381c62da5fc7b16d3585a37ffabd120cbe410
Michael FAGNO <michael.fagno@gmail.com>
2012-12-03 19:42:52 +0100

    next step method done

--- GameOfLife/GOL.hs
+++ GameOfLife/GOL.hs
@@ -10,6 +10,11 @@ type Row a = [a]
 type Value = Bool
 type Point = (Int,Int)
 
+nextStep ::  Grid -> Grid
+nextStep grid = [ [ transform (x,y) grid | y <-[0..nbRows-1] ] | x <-[0..nbLines-1] ]
+								where	nbLines = length grid;
+											nbRows = length $ head grid
+
 getNeighbors :: Point -> Grid -> [Point]
 getNeighbors (x,y) grid = filter (\a -> isInside a grid) neighbor
 												where neighbor = map (\(dx,dy) -> (x+dx,y+dy)) neighborsOffset
@@ -17,10 +22,26 @@ getNeighbors (x,y) grid = filter (\a -> isInside a grid) neighbor
 isInside (a,b) grid | a >= 0 && a < length grid && b >= 0 && b < length (head grid) = True
 										| otherwise = False
 
-neighborsOffset = [(-1,-1),(0,-1),(1,-1),
-             (-1, 0),       (1, 0),
-             (-1, 1),(0, 1),(1, 1)]
+neighborsOffset = [	(-1,-1),(0,-1),(1,-1),
+             				(-1, 0),       (1, 0),
+             				(-1, 1),(0, 1),(1, 1)]
 
- 
+countNextAlive :: Point -> Grid -> Int
+countNextAlive pt grid = length $ filter (\(x,y)->grid!!x!!y==True) (getNeighbors pt grid)
 
-example=[[False,True, False],[False,True, False],[False,True, False]]
+hasToDie pt grid 	| nbAlive  < 2 || nbAlive > 3= True
+									| otherwise = False
+									where nbAlive = countNextAlive pt grid
+
+hasToBorn pt grid | nbAlive  == 3 = True
+									| otherwise = False
+									where nbAlive = countNextAlive pt grid
+
+transform:: Point -> Grid -> Bool
+transform (x,y) grid	| hasToDie pt grid = False
+											| hasToBorn pt grid = True
+											| otherwise = grid!!x!!y
+												where pt = (x,y)
+
+---------------------------------------------------------------------
+example=[[False,False, False],[True,True, True],[False,False, False]]
