69e2e4415dffd4009efe149e286b7dbf96a41135
Michael FAGNO <michael.fagno@gmail.com>
2012-12-03 18:41:30 +0100

    getNeighboor method done

--- GameOfLife/GOL.hs
+++ GameOfLife/GOL.hs
@@ -8,5 +8,19 @@ type Grid = Matrix Value
 type Matrix a = [Row a]
 type Row a = [a]
 type Value = Bool
+type Point = (Int,Int)
 
+getNeighbors :: Point -> Grid -> [Point]
+getNeighbors (x,y) grid = filter (\a -> isInside a grid) neighbor
+												where neighbor = map (\(dx,dy) -> (x+dx,y+dy)) neighborsOffset
 
+isInside (a,b) grid | a >= 0 && a < length grid && b >= 0 && b < length (head grid) = True
+										| otherwise = False
+
+neighborsOffset = [(-1,-1),(0,-1),(1,-1),
+             (-1, 0),       (1, 0),
+             (-1, 1),(0, 1),(1, 1)]
+
+ 
+
+example=[[False,True, False],[False,True, False],[False,True, False]]
